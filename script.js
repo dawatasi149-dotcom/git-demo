e49c7dfdb2a940937ad246b6e205cb7a84e04754
Dawa tashi Sherpa <dawatasi149@gmail.com>
2026-04-10 12:04:56 +0545

    change

--- script.js
+++ script.js
@@ -1,31 +1,9 @@
-// // Array is a collection of a simillear type of data
-// let sum = 0;
-// let marks = [10, 20, 30, 40, 45, 59];
-// console.log("The total marks of a syudent is ", marks.length);
-// for (let i of marks) {
-//     sum += i;
-// }
-// console.log("The total marks of student is ", sum);
-// let avg = sum / marks.length;
-// console.log("The avarage of student is ", avg);
-
-
-//PRICE BEFORE TAX
-// finalPrice = 0;
-// let price = [100, 300, 500, 700];
-// // TAX for all item 10%
-// for (let i of price) {
-//     console.log(`The price before discount ${i}`);
-// }
-// for (let i of price) {
-//     finalPrice = i * 0.1;
-//     console.log(`The price after tax ${finalPrice}`);
-// }
-//PUSH AND PoP IN ARRAY
-let food = ["potato", "cabbage", "chilly"];
-// food.push("mango");
-// console.log(food);
-// food.pop("orange");
-// console.log(food);
-food.toString();
-console.log(food);
+let company = ["ola", "amazon", "microsoft", "hitachi"];
+company.unshift("uer");
+console.log(company);
+company.shift();
+console.log(company);
+company.slice(1, 2);
+console.log(company);
+company.splice(1, 2, "hello");
+console.log(company);
