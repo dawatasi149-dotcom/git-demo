3f9c29cf64c51428c3cc34e6ee7609a0808bab16
Dawa tashi Sherpa <dawatasi149@gmail.com>
2026-04-14 17:20:15 +0545

    Make some changes

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-let sum = 0;
-let a = prompt("Enter your number "); 
-for (let i = 0; i <= a; i++){
-    sum += i;
-}
-console.log(sum);
+//dark mode changer 
+let btn1 = document.querySelector("#btn");
+let curMode = "dark";
+btn1.addEventListener("click", () => {
+    if (curMode === "dark") {
+        curMode = "light";
+         document.querySelector("h1").style.color = "black";
+        document.querySelector("body").style.backgroundColor = "white";
+    }
+    else {
+        curMode = "dark";
+        document.querySelector("h1").style.color = "white";
+        document.querySelector("body").style.backgroundColor = "black";
+    }
+})
