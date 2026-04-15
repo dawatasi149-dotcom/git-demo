83590954e0b19f572df0b32546a8f1f9945d787a
Dawa tashi Sherpa <dawatasi149@gmail.com>
2026-04-15 19:30:32 +0545

    learn object and class in js

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
-//dark mode changer 
-let btn1 = document.querySelector("#btn");
-let curMode = "dark";
-btn1.addEventListener("click", () => {
-    if (curMode === "dark") {
-        curMode = "light";
-         document.querySelector("h1").style.color = "black";
-        document.querySelector("body").style.backgroundColor = "white";
+//class and object in js
+class ToyotaCar{
+    constructor(brand, milage) {
+        console.log("creating a new class");
+        this.brand = brand;
+        this.milage = milage;
     }
-    else {
-        curMode = "dark";
-        document.querySelector("h1").style.color = "white";
-        document.querySelector("body").style.backgroundColor = "black";
+    start() {
+        console.log("start");
+
     }
-})
+    stop() {
+        console.log("stop");
+    }
+}
+class supra extends ToyotaCar {
+    constructor(brand, milage, version) {
+        super(brand, milage);
+        this.version = version;
+    }
+}
+let fortuner = new supra("fortuner", 10, "v10");
+console.log(fortuner);
