81343f18f100359f59b0df6e1b2f4dc41192e993
Dawa tashi Sherpa <dawatasi149@gmail.com>
2026-04-22 18:24:32 +0545

    learn async await in js

--- script.js
+++ script.js
@@ -1,16 +1,67 @@
-function async(dataId) {
+//async await in js 
+function getData(dataId) {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            console.log("data 1 ",dataId);
+            console.log("data", dataId);
             resolve("success");
-        }, 3000);
+        }, 2000);
     });
+};
+async function getAlldata() {
+    await getData(1);
+    console.log("getting data 2");
+    await getData(2);
 }
-async(1).then((res) => {
-    return async(2);
-}).then((res) => {
-    console.log(res);
-})
+
+(async function () {
+    console.log("geting all data 2")
+    await getData(1);
+    console.log("getting data 2");
+    await getData(2);
+})();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function async(dataId) {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             console.log("data",dataId);
+//             resolve("success");
+//         }, 3000);
+//     });
+// }
+// console.log("getting data 1")
+// async(1).then((res) => {
+//       console.log("getting data 2");
+//     return async(2);
+  
+// }).then((res) => {
+//     console.log("getting data 3");
+//     return async(3);
+// }).then((res) => {
+//     console.log(res);
+// })
 
 
 
